4bfc740ea29e309b380080c90180d5c009391638
rashanarshad <Cokedrink12@>
2020-06-26 22:55:47 -0500

    first commit, pushing working scripts and relevant docker image

---
+++ app/main.py
@@ -0,0 +1,122 @@
+# example of inference with a pre-trained coco model
+from keras.preprocessing.image import img_to_array
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from fastapi import FastAPI, Form
+
+import numpy as np
+import cv2
+import base64
+from urllib.parse import quote
+
+# Root directory of the project
+
+# Import Mask RCNN
+from mrcnn import model as modellib
+# Import COCO config
+from mrcnn.config import Config
+from mrcnn.visualize import display_instances
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# draw an image with detected objects
+def draw_image_with_boxes(filename, boxes_list):
+    # load the image
+    data = pyplot.imread(filename)
+    # plot the image
+    pyplot.imshow(data)
+    # get the context for drawing boxes
+    ax = pyplot.gca()
+    # plot each box
+    for box in boxes_list:
+        # get coordinates
+        y1, x1, y2, x2 = box
+        # calculate width and height of the box
+        width, height = x2 - x1, y2 - y1
+        # create the shape
+        rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+        # draw the box
+        ax.add_patch(rect)
+    # show the plot
+    pyplot.show()
+
+
+class TestConfig(Config):
+    NAME = "test"
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 80
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+# define the model
+
+
+def readb64(uri):
+   encoded_data = uri.split(',')[1]
+   nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   return img
+
+
+rcnn = modellib.MaskRCNN(mode="inference", model_dir='../', config=TestConfig())
+
+
+# load coco model weights
+rcnn.load_weights('mask_rcnn_coco.h5', by_name=True)
+rcnn.keras_model._make_predict_function()
+
+@app.post('/')
+async def return_masked(file: str = Form(...)):
+    img64 = file
+    # print(img64)
+    # convert string of image data to uint8
+    img = readb64(img64)
+    img = img_to_array(img)
+
+    # make prediction
+
+    results = rcnn.detect([img], verbose=0)
+    # visualize the results
+
+    # get dictionary for first prediction
+    r = results[0]
+    # show photo with bounding boxes, masks, class labels and scores
+    output_png = display_instances(img, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+    data = output_png.decode('ascii')
+    data_url = 'data:image/jpg;base64,{}'.format(quote(data))
+    return data_url
+
